b21350c9485194fb44136f54d64ea4a6e10daf80
Peterson Paris <parischazhayan.21inf05@gmail.com>
2025-04-22 04:59:59 -0300

    testfree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const toggleButton = document.querySelector('.header__toggle');
+
+const navList = document.querySelector('.nav__list');
+
+toggleButton.addEventListener('click', () => {
+  navList.classList.toggle('is-open');
+
+});
+
