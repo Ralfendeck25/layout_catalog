91d08ea84bf62cd7229e223f559341909f50f525
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-22 05:55:04 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const toggleButton = document.querySelector('.header__toggle');
+
+    const navList = document.querySelector('.nav__list');
+
+    toggleButton?.addEventListener('click', () => {
+      navList.classList.toggle('is-open');
+
+    });
