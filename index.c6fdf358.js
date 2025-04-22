8865ff63efea44956c50fb36a56fbe43477afa67
Peterson Paris <parischazhayan.21inf05@gmail.com>
2025-04-22 07:05:31 -0300

    make build

---
+++ index.c6fdf358.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__toggle"),t=document.querySelector(".nav__list");e?.addEventListener("click",()=>{t.classList.toggle("is-open")});
+//# sourceMappingURL=index.c6fdf358.js.map
